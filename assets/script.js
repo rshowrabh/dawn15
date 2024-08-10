2735c0d7c933d436ab1b23b11bc9c9445965d69e
rshowrabh <48219209+rshowrabh@users.noreply.github.com>
2024-08-10 16:28:36 +0600

    imo

--- assets/script.js
+++ assets/script.js
@@ -302,5 +302,5 @@ window.addEventListener('DOMContentLoaded', () => {
 // Read More Less Function
 $('.readmore').click(function (event) {
   $(this).closest('.product-description-full').css("display","block");
-  $(this).closest('.product-description-short').css("display","none");
+  // $(this).closest('.product-description-short').css("display","none");
   });
